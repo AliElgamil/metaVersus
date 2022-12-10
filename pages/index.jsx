3238a97aeef8a81d8b865febeb0d41d36d6f0ac7
Ali Elgamel <alielgamil345@gmail.com>
2022-12-10 15:44:13 +0200

    show

--- pages/index.jsx
+++ pages/index.jsx
@@ -41,35 +41,32 @@ const World = dynamic(() => import('../sections/World'), {
 const Page = () => {
   const { scrollYProgress } = useScroll();
   return (
-    <Suspense fallback={<Loading />}>
-      <>
-        <Loading show />
-        <div className="bg-primary-black overflow-hidden">
-          <motion.div
-            style={{ scaleX: scrollYProgress }}
-            className="h-[5px] w-full bg-[#a509ff] fixed top-0 left-0 origin-left z-50"
-          />
-          <Navbar />
-          <Hero />
-          <div className="relative">
-            <About />
-            <div className="gradient-03 z-0" />
-            <Explore />
-          </div>
-          <div className="relative">
-            <GetStarted />
-            <div className="gradient-04 z-0" />
-            <WhatsNew />
-          </div>
-          <World />
-          <div className="relative">
-            <Insights />
-            <div className="gradient-04 z-0" />
-            <Feedback />
-          </div>
-          <Footer />
+    <Suspense fallback={<Loading show />}>
+      <div className="bg-primary-black overflow-hidden">
+        <motion.div
+          style={{ scaleX: scrollYProgress }}
+          className="h-[5px] w-full bg-[#a509ff] fixed top-0 left-0 origin-left z-50"
+        />
+        <Navbar />
+        <Hero />
+        <div className="relative">
+          <About />
+          <div className="gradient-03 z-0" />
+          <Explore />
         </div>
-      </>
+        <div className="relative">
+          <GetStarted />
+          <div className="gradient-04 z-0" />
+          <WhatsNew />
+        </div>
+        <World />
+        <div className="relative">
+          <Insights />
+          <div className="gradient-04 z-0" />
+          <Feedback />
+        </div>
+        <Footer />
+      </div>
     </Suspense>
   );
 };
